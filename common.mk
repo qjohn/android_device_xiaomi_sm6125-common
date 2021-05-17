eca4d96403fa64991c9f03a54663ddb6445c0677
DarkJoker360 <simoespo159@gmail.com>
2021-05-17 23:34:02 +0200

    sm6125-common: Uprev btconfigstore to 2.0

--- common.mk
+++ common.mk
@@ -144,7 +144,7 @@ PRODUCT_BOOT_JARS += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
