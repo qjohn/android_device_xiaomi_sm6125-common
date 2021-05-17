948a629479e7f046dae9602339ee8f6d8b4a7f1c
DarkJoker360 <simoespo159@gmail.com>
2021-05-17 23:43:13 +0200

    sm6125-common: Update camera packages for R

--- common.mk
+++ common.mk
@@ -151,12 +151,15 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
     android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
